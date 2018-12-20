563b744359b0d9cc3a307988a7f30d9d15679ae7
Matthew Werdean <mwerdean@gmail.com>
2018-12-20 15:13:39 -0700

    Custom Slack Messages

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const request = require('request-promise')
 const dotenv = require('dotenv').config()
+const express  = require('express')
+const app = express()
+const hook = process.env.HOOK
 
-const hook = process.env.hook
-
-const getData = async function() {
+ async function getData() {
     const json = await request({
         url: 'http://next.json-generator.com/api/json/get/NJmmhK3pV',
         json: true
@@ -16,23 +17,39 @@ const getData = async function() {
         lastName: person.name.last
     }))
 }
-(async function () {
+
+(async function() {
 
     try {
         // get the data 
         const people = await getData()
         console.log(people)
 
-        const slackBody= {
+        const slackBody = {
+            mkdwn: true,
             text: `This is a really great slack message!`,
+            attachments: people.map(person => ({
+                color: 'good',
+                text: `*${person.email}* and their name is ${person.firstName} ${person.lastName}, and they are ${person.age}}`
+            }))
         }
 
         // post to slack
-        request({
-            metho: 'post'
+        const res = await request({
+            url: `https://hooks.slack.com/services/${hook}`,
+            method: 'post',
+            body: slackBody,
+            json: true
         })
+
     } catch (e) {
         console.log('our error', e)
     }
     debugger
+}());
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
 })
